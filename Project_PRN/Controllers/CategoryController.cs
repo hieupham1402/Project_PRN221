466e3f03f40fdda9623597b1a6bafdc0f051c6d0
hieupham1402 <huy793092@gmail.com>
2023-11-09 14:10:37 +0700

    first commit

---
+++ Project_PRN/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_PRN.Data;
+using Project_PRN.Models;
+
+namespace Project_PRN.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
